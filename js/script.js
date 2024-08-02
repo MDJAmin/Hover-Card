194fdfe450f7e37d8cd95c7df92874ed4af1a143
MDJAmin <mohammadamindf@gmail.com>
2024-08-02 18:09:15 +0430

    dynamic the card numbers

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("mouseover", function () {
+    let percentage = card.querySelector(".percentage");
+    let current = 0;
+    let target = 85;
+
+    const interval = setInterval(() => {
+      if (current <= target) {
+        percentage.textContent = `${current}%`;
+        current++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 5);
+  });
+});
